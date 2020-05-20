9c8843ebb85a0ef69eadbc1d923c5bba583f45d4
tachodril <rajlevel97@gmail.com>
2020-05-20 23:53:34 +0530

    socket.io-client init

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+
+const PORT = process.env.PORT || 5000;
+
+const router = require("./router");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", socket => {
+  console.log("We have a new connection !!!");
+
+  socket.on("disconnect", () => {
+    console.log("User has left !!!");
+  });
+});
+
+app.use(router);
+
+server.listen(PORT, () =>
+  console.log("Server running fine !! on port ${PORT}")
+);
